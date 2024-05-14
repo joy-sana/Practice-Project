ea1cf29da171e51e5192680916b4a9dfaafbdaa0
Joy Abdin Sanai <zjoy.311@gmail.com>
2024-05-14 23:00:42 +0530

    commit

---
+++ src/myPackage/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myPackage;
+
+/**
+ *
+ * @author joy
+ */
+public class NewClass {
+    public int a;
+    public String b ;
+    NewClass() {
+        a = 0;
+        b = null;
+    }
+}
