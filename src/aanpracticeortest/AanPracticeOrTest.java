4f4b8410a6d7f88c489fe91b8ea9403f53b81324
Joy Abdin Sanai <zjoy.311@gmail.com>
2024-05-14 22:32:22 +0530

    new package and class added

--- src/aanpracticeortest/AanPracticeOrTest.java
+++ src/aanpracticeortest/AanPracticeOrTest.java
@@ -2,21 +2,7 @@ package aanpracticeortest;
 
 import java.util.Arrays;
 
-abstract class C1{
-public C1(){
-System.out.print(1);
-}
-}
-class C2 extends C1{
-public C2(){
-System.out.print(2);
-}
-}
-class C3 extends C2{
-public C3(){
-System.out.println(3);
-}
-}
+
 public class AanPracticeOrTest {
     public static void main(String[] args) {
 //        int[] digits = {9,8,7,6,5,4,3,2,1,0};
@@ -87,7 +73,7 @@ public class AanPracticeOrTest {
 
 
 
-//int num = ;/
+int num = 2 ;
 
 System.out.println(num/2);
        }
