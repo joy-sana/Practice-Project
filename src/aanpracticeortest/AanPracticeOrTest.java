139d298b1bfc598966ab968718c925307bdcb0d5
Joy Abdin Sanai <zjoy.311@gmail.com>
2024-05-14 21:00:20 +0530

    commit

---
+++ src/aanpracticeortest/AanPracticeOrTest.java
@@ -0,0 +1,96 @@
+package aanpracticeortest;
+
+import java.util.Arrays;
+
+abstract class C1{
+public C1(){
+System.out.print(1);
+}
+}
+class C2 extends C1{
+public C2(){
+System.out.print(2);
+}
+}
+class C3 extends C2{
+public C3(){
+System.out.println(3);
+}
+}
+public class AanPracticeOrTest {
+    public static void main(String[] args) {
+//        int[] digits = {9,8,7,6,5,4,3,2,1,0};
+//        long num = 0;
+//        for (int digit: digits){
+//            num = num * 10 + (int)digit;
+//        }
+//        System.out.println(num);
+//        num++;
+//        System.out.println(num);
+//        
+//        
+//         String str = String.valueOf(num);
+//        int[] newArr = new int[str.length()];
+//
+//    for (int i = 0; i < str.length() ;i++){
+//
+//        newArr[i] = Character.getNumericValue(str.charAt(i));
+//    }
+//    System.out.println(Arrays.toString(newArr));
+
+//            System.out.println();
+
+//               int[]  x = {120, 077, 016};
+//               for(int i = 0; i < x.length; i++){
+//                        System.out.print(x[i] + " ");
+//               }  
+
+
+//int[] A = {0,2,4,1,3};
+//for(int i = 0; i < A.length; i++){
+//int k = (A[i] + 3) % A.length;
+//A[i] = A[k];
+//System.out.println(k+ " ** "+ A[k]);
+//}
+//System.out.println(A[1]);
+//System.out.println("**********");
+//System.out.println(7%5);
+
+
+
+// boolean flag = false;
+//        for (int i = 0; i< s.length(); i++){
+//            if (s[i]== "(" && s[i+1] == ")"){
+//                flag = true;
+//            }
+//            else{
+//                return false;
+//            }
+//            if (s[i]== "{" && s[i+1] == "}"){
+//                flag = true;
+//            }
+//            else{
+//                return false;
+//            }
+//            if (s[i]== "[" && s[i+1] == "]"){
+//                flag = true;
+//            }
+//            else{
+//                return false;
+//            }
+//        }
+//        return flag == true;
+//        
+
+
+
+
+
+
+//int num = ;/
+
+System.out.println(num/2);
+       }
+}
+    
+
